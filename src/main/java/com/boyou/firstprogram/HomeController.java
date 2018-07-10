8e6d603f123b60d3b83ef2f8c0b511538dc246aa
Joey <draganicx@gmail.com>
2018-07-09 20:15:11 -0400

    fix2

--- src/main/java/com/boyou/firstprogram/HomeController.java
+++ src/main/java/com/boyou/firstprogram/HomeController.java
@@ -1,13 +1,16 @@
 package com.boyou.firstprogram;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String homePage(){
+    public String homePage(Model model)
+    {
+        model.addAttribute("myvariable", "123");
         return"index";
     }
 
